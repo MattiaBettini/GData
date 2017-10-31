79762f27d494acce09f18b8a77e087cf8b89d7b7
Mattia Bettini <matt31996@gmail.com>
2017-10-31 15:33:37 +0100

    Change .gitgnore for support C# Code Added

---
+++ GData/GData/GData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GData
+{
+    public static class GData
+    {
+        public static T Load<T>(string path)
+            where T : class, new()
+        {
+            T instance = new T();
+            using (Stream stream = File.Open(path, FileMode.Open))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                instance = (T)formatter.Deserialize(stream);
+
+                return instance;
+            }
+        }
+
+        public static void Save<T>(T objtosave, string path)
+            where T : class, new()
+        {
+            using (Stream stream = File.Open(path, FileMode.OpenOrCreate))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, objtosave);
+            }
+        }
+
+    }
+}
