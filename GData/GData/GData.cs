f175835d39c12e2003ff78e55788989439dab7a0
Mattia Bettini <matt31996@gmail.com>
2017-11-14 18:47:54 +0100

    Add Xml Mode For Save And Load

--- GData/GData/GData.cs
+++ GData/GData/GData.cs
@@ -5,12 +5,23 @@ using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace GData
 {
     public static class GData
     {
-        public static T Load<T>(string path)
+        public static T Load<T>(string path,ModeType mode)
+            where T : class, new()
+        {
+            if (mode == ModeType.Binary)
+                return BinaryLoad<T>(path);
+            else
+                return XmlLoad<T>(path);
+        }
+
+        #region Load Implementation
+        private static T BinaryLoad<T>(string path)
             where T : class, new()
         {
             T instance = new T();
@@ -23,7 +34,31 @@ namespace GData
             }
         }
 
-        public static void Save<T>(T objtosave, string path)
+        private static T XmlLoad<T>(string path)
+            where T : class, new()
+        {
+            T instance = new T();
+            using (Stream stream = File.Open(path, FileMode.Open))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                instance = (T)serializer.Deserialize(stream);
+
+                return instance;
+            }
+        }
+        #endregion
+
+        public static void Save<T>(T objtosave, string path, ModeType mode)
+            where T : class, new()
+        {
+            if (mode == ModeType.Binary)
+                BinarySave<T>(objtosave, path);
+            else
+                XmlSave<T>(objtosave, path);
+        }
+
+        #region Save Implementation
+        private static void BinarySave<T>(T objtosave, string path)
             where T : class, new()
         {
             using (Stream stream = File.Open(path, FileMode.OpenOrCreate))
@@ -32,6 +67,17 @@ namespace GData
                 formatter.Serialize(stream, objtosave);
             }
         }
+
+        private static void XmlSave<T>(T objtosave, string path)
+            where T : class, new()
+        {
+            using (Stream stream = File.Open(path, FileMode.OpenOrCreate))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                serializer.Serialize(stream, objtosave);
+            }
+        }
+        #endregion
 
     }
 }
